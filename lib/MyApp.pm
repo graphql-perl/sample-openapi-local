669f251d8d79acc1aad95b03d97764f6915bb4c9
Ed J <mohawk2@users.noreply.github.com>
2018-02-26 05:17:05 +0000

    minimal graphql interface

--- lib/MyApp.pm
+++ lib/MyApp.pm
@@ -1,11 +1,14 @@
 package MyApp;
 use Mojo::Base "Mojolicious";
+use Mojo::File 'path';
 
 sub startup {
   my $self = shift;
 
   # Load the "api.yaml" specification from the public directory
-  $self->plugin(OpenAPI => {spec => $self->static->file("api.yaml")->path});
+  my $path = path($self->static->file("api.yaml")->path)->realpath;
+  my $api = $self->plugin(OpenAPI => {spec => $path});
+  $self->plugin(GraphQL => {convert => [ qw(OpenAPI), $api->validator->bundle({ref_key => 'definitions'}), $self ], graphiql => 1});
 }
 
 1;
--- t/basic.t
+++ t/basic.t
@@ -5,5 +5,10 @@ use Test::Mojo;
 
 my $t = Test::Mojo->new('MyApp');
 $t->post_ok('/api/echo', json =>{i=>"work"})->json_is({i=>"work"});
+$t->post_ok('/graphql', json => {
+  query => 'mutation m {echo(body: [{key:"one", value:"two"}]) { key value }}'
+})->json_is(
+  { data => { echo => [{key=>"one", value=>"two"}] } },
+)->or(sub { diag explain $t->tx->res->body });
 
 done_testing();
