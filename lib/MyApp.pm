440166311a6d4fa59516050e6950379dabbe708c
Ed J <mohawk2@users.noreply.github.com>
2018-03-10 04:57:38 +0000

    zap code forced by JV pre-2.04

--- lib/MyApp.pm
+++ lib/MyApp.pm
@@ -6,9 +6,9 @@ sub startup {
   my $self = shift;
 
   # Load the "api.yaml" specification from the public directory
-  my $path = path($self->static->file("api.yaml")->path)->realpath;
+  my $path = $self->static->file("api.yaml")->path;
   my $api = $self->plugin(OpenAPI => {spec => $path});
-  $self->plugin(GraphQL => {convert => [ qw(OpenAPI), $api->validator->bundle({ref_key => 'definitions'}), $self ], graphiql => 1});
+  $self->plugin(GraphQL => {convert => [ qw(OpenAPI), $api->validator->bundle, $self ], graphiql => 1});
 }
 
 1;
